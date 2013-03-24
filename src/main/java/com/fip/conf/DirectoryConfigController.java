7354d444bf6c05c8f020e7404e5882f117a9c245
Fabien Ipseiz <f.ipseiz@gmail.com>
2013-03-24 08:53:12 +0100

    Add method createDirectory
    This method check if the entered file path is an existing directory, and
    if the directory does not exist propose to create the directory

--- src/main/java/com/fip/conf/DirectoryConfigController.java
+++ src/main/java/com/fip/conf/DirectoryConfigController.java
@@ -1,6 +1,19 @@
 package com.fip.conf;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // DirectoryConfigController.java
 // Handles user interaction with listeners.
@@ -14,6 +27,10 @@ public class DirectoryConfigController implements ButtonsListener {
 
 	private final DirectoryPair dirPair;
 	private final DirectoryConfigView dirConfView;
+	
+	/** SLF4J bound to logback-classic. */
+	private static final Logger logger = LoggerFactory.getLogger(DirectoryConfigController.class);
+	
 
 	public DirectoryConfigController(DirectoryPair pair, DirectoryConfigView view) {
 		this.dirPair = pair;
@@ -24,20 +41,57 @@ public class DirectoryConfigController implements ButtonsListener {
 	
 	@Override
 	public void okPerformed(ActionEvent e) {
-		dirPair.setSrc(dirConfView.getSrcText());
-		dirPair.setTgt(dirConfView.getTgtText());
+		String srcPath = dirConfView.getSrcText();
+		String tgtPath = dirConfView.getTgtText();
+		try {
+			createDirectory(srcPath);
+			createDirectory(tgtPath);
+		} catch (IOException ioe){
+			logger.error("An error occurs during Directory creation process");
+		}
+		
+		dirPair.setSrc(srcPath);
+		dirPair.setTgt(tgtPath);
 
 		System.out.println("source directory : " + dirPair.getSrc() + "\n"
 				+ "target directory : " + dirPair.getTgt());
-		
-		// TODO check if the directories exist
-		
 	}
 	
 	@Override
 	public void cancelPerformed(ActionEvent e) {
-		
-	}
-	
-}
 
+	}
+
+	/**
+	 * Check if a specified file path is a folder and create a folder if it does
+	 * not exist.
+	 * 
+	 * @param folderPath
+	 *            A folder path.
+	 */
+	public static final void createDirectory(String folderPath) throws IOException {
+		final Path folder = Paths.get(folderPath);
+		
+		if (Files.notExists(folder)) {
+			// Get translation object: 
+			TextTranslation t = TextTranslation.getInstance();
+			
+			// Create dialog panel:
+			JPanel panel = new JPanel(new GridLayout(3, 1));
+			JLabel msg = new JLabel(t.get("profile.dir.create.message"));
+			JLabel question = new JLabel(t.get("profile.dir.create.question"));
+			JTextField directory = new JTextField(folderPath);
+			panel.add(msg);
+			panel.add(question);
+			panel.add(directory);
+
+			int selection = JOptionPane.showConfirmDialog(null, panel,
+					t.get("profile.dir.create.title"),
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+
+			if (selection == JOptionPane.OK_OPTION) {
+				Files.createDirectory(folder);
+			}
+		}
+	}
+}
